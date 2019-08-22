58343b90cbb553a22daf0bb58c0294b6c8401685
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2019-08-22 14:28:52 -0400

    TEMP assertion failures on invalid pthread calls

--- deps/RediSearch/src/rwlock.h
+++ deps/RediSearch/src/rwlock.h
@@ -5,8 +5,8 @@
 
 extern pthread_rwlock_t RWLock;
 
-#define RWLOCK_ACQUIRE_READ() pthread_rwlock_rdlock(&RWLock)
-#define RWLOCK_ACQUIRE_WRITE() pthread_rwlock_wrlock(&RWLock)
-#define RWLOCK_RELEASE() pthread_rwlock_unlock(&RWLock)
+#define RWLOCK_ACQUIRE_READ() assert(pthread_rwlock_rdlock(&RWLock) == 0)
+#define RWLOCK_ACQUIRE_WRITE() assert(pthread_rwlock_wrlock(&RWLock) == 0)
+#define RWLOCK_RELEASE() assert(pthread_rwlock_unlock(&RWLock) == 0)
 
 #endif /* SRC_RWLOCK_H_ */
